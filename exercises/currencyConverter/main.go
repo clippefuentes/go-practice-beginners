cb260ce2c906b076b0adea18e54dd9a9238fd0e8
clippefuentes <clippefuentes@gmail.com>
2021-09-14 22:16:10 -0600

    add exercise

---
+++ exercises/currencyConverter/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Currency Converter
+//
+//   In this exercise, you're going to display currency exchange ratios
+//   against USD.
+//
+//   1. Declare a few constants with iota. They're going to be the keys
+//      of the array.
+//
+//   2. Create an array that contains the conversion ratios.
+//
+//      You should use keyed elements and the contants you've declared before.
+//
+//   3. Get the USD amount to be converted from the command line.
+//
+//   4. Handle the error cases for missing or invalid input.
+//
+//   5. Print the exchange ratios.
+//
+// EXPECTED OUTPUT
+//   go run main.go
+//     Please provide the amount to be converted.
+//
+//   go run main.go invalid
+//     Invalid amount. It should be a number.
+//
+//   go run main.go 10.5
+//     10.50 USD is 9.24 EUR
+//     10.50 USD is 8.19 GBP
+//     10.50 USD is 1186.71 JPY
+//
+//   go run main.go 1
+//     1.00 USD is 0.88 EUR
+//     1.00 USD is 0.78 GBP
+//     1.00 USD is 113.02 JPY
+// ---------------------------------------------------------
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Please provide the amount to be converted")
+		return
+	}
+
+	v, err := strconv.ParseFloat(os.Args[1], 64)
+
+	if err != nil {
+		fmt.Println("Invalid amount. It should be a number.")
+		return
+	}
+	const (
+		EUR = iota
+		GBP
+		JPY
+	)
+
+	ratioStrings := [...]string{
+		"EUR", "GBP", "JPY",
+	}
+
+	ratios := [...]float64{
+		EUR: 0.88,
+		GBP: 0.78,
+		JPY: 113.02,
+	}
+
+	for i, r := range ratios {
+		fmt.Printf("%.2f USD is %.2f %s \n", float64(v), float64(v)*r, ratioStrings[i])
+	}
+}
---
+++ exercises/getAverage/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Find the Average
+//
+//  Your goal is to fill an array with numbers and find the average.
+//
+//  1. Get the numbers from the command-line.
+//
+//  2. Create an array and assign the given numbers to that array.
+//
+//  3. Print the given numbers and their average.
+//
+// RESTRICTION
+//   + Maximum 5 numbers can be provided
+//   + If one of the arguments are not a valid number, skip it
+//
+// EXPECTED OUTPUT
+//   go run main.go
+//     Please tell me numbers (maximum 5 numbers).
+//
+//   go run main.go 1 2 3 4 5 6
+//     Please tell me numbers (maximum 5 numbers).
+//
+//   go run main.go 1 2 3 4 5
+//     Your numbers: [1 2 3 4 5]
+//     Average: 3
+//
+//   go run main.go 1 a 2 b 3
+//     Your numbers: [1 0 2 0 3]
+//     Average: 2
+// ---------------------------------------------------------
+
+func main() {
+	args := os.Args[1:]
+	if len(args) > 5 || len(args) < 0 {
+		fmt.Println("Please tell me numbers (maximum 5 numbers).")
+		return
+	}
+
+	argsNumber := [5]int{}
+	total := 0
+	avg := 0
+
+	for i, v := range args {
+		numArgs, err := strconv.Atoi(v)
+		if err != nil {
+			continue
+		} else {
+			argsNumber[i] = numArgs
+			avg++
+		}
+	}
+
+	fmt.Println("Your numbers: ", argsNumber)
+	
+	for _, v := range argsNumber {
+		total += v
+	}
+
+	fmt.Printf("Average: %d", total/avg)
+}
